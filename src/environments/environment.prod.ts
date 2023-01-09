e2462aac9673d0af93fe6160174e2b55d58d194c
Teikidisi <robyperalta745@gmail.com>
2023-01-09 08:47:17 -0800

    added env to prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  SUPABASE_URL: 'https://sbzdxkeneqfjjzypgkng.supabase.co',
+  SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNiemR4a2VuZXFmamp6eXBna25nIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njc0MjMxNTEsImV4cCI6MTk4Mjk5OTE1MX0.IZmoPW_qU67FlpUuLqNcfO9pefZTsocvbJSPDBEQRqY'
+
 };

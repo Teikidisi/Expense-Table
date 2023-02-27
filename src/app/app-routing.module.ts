7ac5810eed279c1f8313e47542a33e3e119b79cd
Teikidisi <robyperalta745@gmail.com>
2023-02-27 15:25:54 -0800

    modify routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 const routes: Routes = [
   {path: 'main', component: MainViewComponent, title:'Money Expenditure Tracker'},
   {path:'login', component: LogInScreenComponent, title:'Login to App'},
-  {path: '', redirectTo: '/main', pathMatch: 'full'},
+  {path: '', redirectTo: '/login', pathMatch:'prefix' },
   {path:'**', component: PageNotFoundComponent},
 ]
 

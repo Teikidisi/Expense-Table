81ae537fbd02c1d391a4cec8e2064acd08b2b6c9
Teikidisi <robyperalta745@gmail.com>
2023-02-28 15:35:04 -0800

    CSS responsive for table page

--- src/app/Helpers/main-validator.ts
+++ src/app/Helpers/main-validator.ts
@@ -1,4 +1,10 @@
-export class MainValidator {
+import {NgForm} from '@angular/forms';
+
+export default class MainValidator {
     constructor (){}
     
+
+    // public validate(form:NgForm): boolean {
+
+    // }
 }
